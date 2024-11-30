44c87ce7b2bf0f0acd03492d6405a8ad2b290c5a
Nkot117 <kumakura117@gmail.com>
2024-11-30 18:31:21 +0900

    registerメソッドのUnitTestを作成

--- core/data/src/test/kotlin/com/nkot117/data/AuthRepositoryUnitTest.kt
+++ core/data/src/test/kotlin/com/nkot117/data/AuthRepositoryUnitTest.kt
@@ -3,6 +3,7 @@ package com.nkot117.data
 import com.nkot117.data.model.auth.LoginData
 import com.nkot117.data.preferences.TokenManager
 import com.nkot117.data.model.Result
+import com.nkot117.data.model.auth.RegisterData
 import com.nkot117.data.model.auth.UserData
 import com.nkot117.data.repository.AuthRepository
 import com.nkot117.data.repository.AuthRepositoryImpl
@@ -10,12 +11,14 @@ import com.nkot117.network.SyncnoteServerApi
 import com.nkot117.network.model.login.LoginRequest
 import com.nkot117.network.model.login.LoginResponse
 import com.nkot117.network.model.login.UserInfo
+import com.nkot117.network.model.register.RegisterRequest
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.types.shouldBeInstanceOf
 import io.mockk.coEvery
+import io.mockk.coVerify
 import io.mockk.mockk
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.ResponseBody.Companion.toResponseBody
@@ -41,8 +44,8 @@ class AuthRepositoryUnitTest : FunSpec({
         val email = "test@test.com"
         val password = "password"
 
-        fun createLoginParams() : LoginRequest {
-           return LoginData(email, password).toNetworkRequest()
+        fun createLoginParams(): LoginRequest {
+            return LoginData(email, password).toNetworkRequest()
         }
 
         context("正常系") {
@@ -60,6 +63,9 @@ class AuthRepositoryUnitTest : FunSpec({
                 val result = repository.login(email, password)
 
                 // Assert
+                coVerify(exactly = 1) { mockTokenManager.saveAccessToken(response.accessToken) }
+                coVerify(exactly = 1) { mockTokenManager.saveRefreshToken(response.refreshToken) }
+
                 result.shouldBeInstanceOf<Result.Success<UserData>>()
                 result.data.name shouldBe "test user"
                 result.data.email shouldBe "test@test.com"
@@ -79,7 +85,10 @@ class AuthRepositoryUnitTest : FunSpec({
                 """.trimIndent()
                     val responseBody = errorJson.toResponseBody("application/json".toMediaType())
 
-                    coEvery { mockApi.login(requestParams) } returns Response.error(500, responseBody)
+                    coEvery { mockApi.login(requestParams) } returns Response.error(
+                        500,
+                        responseBody
+                    )
 
                     // Act
                     val result = repository.login(email, password)
@@ -134,6 +143,93 @@ class AuthRepositoryUnitTest : FunSpec({
                 // Assert
                 result.shouldBeInstanceOf<Result.Failure>()
                 result.errorMessage.message shouldBe "ログインに失敗しました。\nしばらく時間をおいてから、もう一度お試しください。"
+            }
+        }
+    }
+
+    context("registerのテスト") {
+        // Common Arrange
+        val name = "test user"
+        val email = "test@test.com"
+        val password = "password"
+
+        fun createRegisterParams(): RegisterRequest {
+            return RegisterData(name, email, password).toNetworkRequest()
+        }
+
+        context("正常系") {
+            test("ユーザー作成が成功した場合、Result.Successが返却されること") {
+                // Arrange
+                val requestParams = createRegisterParams()
+
+                coEvery { mockApi.register(requestParams) } returns Response.success(null)
+
+                // Act
+                val result = repository.register(name, email, password)
+
+                // Assert
+                result.shouldBeInstanceOf<Result.Success<Unit>>()
+            }
+        }
+
+        context("異常系") {
+            context("サーバーからエラーが返却") {
+                test("レスポンスボディにエラーメッセージが含まれている場合、含まれているエラーメッセージが返却されること") {
+                    // Arrange
+                    val requestParams = createRegisterParams()
+
+                    val errorJson = """
+                    {
+                    "message": "error message",
+                    "reason" : "error reason"
+                    }
+                """.trimIndent()
+                    val responseBody = errorJson.toResponseBody("application/json".toMediaType())
+
+
+                    coEvery { mockApi.register(requestParams) } returns Response.error(
+                        500,
+                        responseBody
+                    )
+
+                    // Act
+                    val result = repository.register(name, email, password)
+
+                    // Assert
+                    result.shouldBeInstanceOf<Result.Failure>()
+                    result.errorMessage.message shouldBe "error message"
+                    result.errorMessage.reason shouldBe "error reason"
+                }
+
+                test("レスポンスボディがない場合、既定のエラーメッセージが返却されること") {
+                    // Arrange
+                    val requestParams = createRegisterParams()
+
+                    coEvery { mockApi.register(requestParams) } returns Response.error(
+                        500,
+                        mockk(relaxed = true)
+                    )
+
+                    // Act
+                    val result = repository.register(name, email, password)
+
+                    // Assert
+                    result.shouldBeInstanceOf<Result.Failure>()
+                    result.errorMessage.message shouldBe "ユーザー登録に失敗しました。\nしばらく時間をおいてから、もう一度お試しください。"
+                }
+            }
+            context("処理中に異常終了した場合、既定のエラーメッセージが返却されること") {
+                // Arrange
+                val requestParams = createRegisterParams()
+
+                coEvery { mockApi.register(requestParams) } throws RuntimeException("Network error")
+
+                // Act
+                val result = repository.register(name, email, password)
+
+                // Assert
+                result.shouldBeInstanceOf<Result.Failure>()
+                result.errorMessage.message shouldBe "ユーザー登録に失敗しました。\nしばらく時間をおいてから、もう一度お試しください。"
             }
         }
     }
