97c7bf5bc81f500e272c610dc501c54c97781a4e
Nkot117 <kumakura117@gmail.com>
2024-12-01 15:42:16 +0900

    createMemoメソッドのUnitTestを作成

--- core/data/src/test/kotlin/com/nkot117/data/MemoRepositoryUnitTest.kt
+++ core/data/src/test/kotlin/com/nkot117/data/MemoRepositoryUnitTest.kt
@@ -505,5 +505,161 @@ class MemoRepositoryUnitTest : FunSpec({
             }
         }
     }
+    context("createMemoのテスト") {
+        // Common Arrange
+        fun createMemoDetailResponse(): MemoDetailResponse {
+            return MemoDetailResponse(
+                id = memoInfo.id,
+                title = memoInfo.title,
+                content = memoInfo.content
+            )
+        }
+        context("正常系") {
+            test("メモの更新が成功した場合、メモ詳細データが返却されること") {
+                // Arrange
+                val response = createMemoDetailResponse()
+
+                coEvery { mockTokenManager.getAccessToken() } returns "Access Token"
+                coEvery { mockApi.createMemo(any(), any()) } returns Response.success(response)
+
+                // Act
+                val result = repository.createMemo(memoInfo.title, memoInfo.content)
+
+                // Assert
+                result.shouldBeInstanceOf<Result.Success<MemoData>>()
+                result.data.id.shouldBe(memoInfo.id)
+                result.data.title.shouldBe(memoInfo.title)
+                result.data.content.shouldBe(memoInfo.content)
+            }
+        }
+
+        context("異常系") {
+            test("アクセストークが保存されていない場合は既定のエラーメッセージが返却されること") {
+                // Arrange
+                coEvery { mockTokenManager.getAccessToken() } returns null
+
+                // Act
+                val result = repository.createMemo(memoInfo.title, memoInfo.content)
+
+                // Assert
+                result.shouldBeInstanceOf<Result.Failure>()
+                result.errorMessage.message.shouldBe("Token not found")
+            }
+
+            test("APIが成功を返してもレスポンスボディがnullの場合、既定のエラーメッセージが返却されること") {
+                // Arrange
+                coEvery { mockTokenManager.getAccessToken() } returns "Access Token"
+                coEvery { mockApi.createMemo(any(), any()) } returns Response.success(null)
+
+                // Act
+                val result = repository.createMemo(memoInfo.title, memoInfo.content)
+
+                // Assert
+                result.shouldBeInstanceOf<Result.Failure>()
+                result.errorMessage.message.shouldBe("Unknown error")
+            }
+
+            context("サーバーからエラーが返却") {
+                test("トークン期限切れの場合、トークンリフレッシュ後に再試行されること") {
+                    // Arrange
+                    val expiredToken = "Expired Token"
+                    val refreshToken = RefreshTokenData("Refresh Token")
+                    val newToken = "New Token"
+
+                    val getMemoDetailResponse = createMemoDetailResponse()
+                    val refreshTokenResponse = RefreshTokenResponse(newToken)
+                    val requestParams = MemoData(
+                        memoInfo.id,
+                        memoInfo.title,
+                        memoInfo.content
+                    )
+
+                    val errorJson = """{
+                        "message": "Token expired",
+                        "reason": "expired"
+                        }""".trimIndent()
+
+                    val responseBody = errorJson.toResponseBody("application/json".toMediaType())
+
+                    coEvery { mockTokenManager.getAccessToken() } returns expiredToken andThen newToken
+
+                    // 期限切れトークンが設定された場合、トークン期限切れエラーを返す
+                    coEvery { mockApi.createMemo("Bearer $expiredToken", requestParams.toNetworkRequest()) } returns Response.error(
+                        401,
+                        responseBody
+                    )
+                    // 有効トークンが設定された場合、正常のレスポンスを返す
+                    coEvery { mockApi.createMemo("Bearer $newToken", requestParams.toNetworkRequest()) } returns Response.success(
+                        getMemoDetailResponse
+                    )
+
+                    coEvery { mockTokenManager.getRefreshToken() } returns refreshToken
+                    coEvery { mockApi.refreshToken(refreshToken.toNetworkRequest()) } returns Response.success(
+                        refreshTokenResponse
+                    )
+
+                    // Act
+                    val result = repository.createMemo(memoInfo.title, memoInfo.content)
+
+                    // Assert
+                    result.shouldBeInstanceOf<Result.Success<MemoData>>()
+                    coVerify(exactly = 1) { mockApi.createMemo("Bearer $expiredToken", requestParams.toNetworkRequest()) }
+                    coVerify(exactly = 1) { mockApi.createMemo("Bearer $newToken", requestParams.toNetworkRequest()) }
+                }
+
+
+                test("レスポンスボディにエラーメッセージが含まれている場合、含まれているエラーメッセージが返却されること") {
+                    // Arrange
+                    val errorJson = """
+                    {
+                    "message": "error message",
+                    "reason" : "error reason"
+                    }
+                """.trimIndent()
+                    val responseBody = errorJson.toResponseBody("application/json".toMediaType())
+
+                    coEvery { mockApi.createMemo(any(), any()) } returns Response.error(
+                        500,
+                        responseBody
+                    )
+
+                    // Act
+                    val result = repository.createMemo(memoInfo.title, memoInfo.content)
+
+                    // Assert
+                    result.shouldBeInstanceOf<Result.Failure>()
+                    result.errorMessage.message shouldBe "error message"
+                    result.errorMessage.reason shouldBe "error reason"
+                }
+
+                test("レスポンスボディがない場合、既定のエラーメッセージが返却されること") {
+                    // Arrange
+                    coEvery { mockApi.createMemo(any(), any()) } returns Response.error(
+                        500,
+                        mockk(relaxed = true)
+                    )
+
+                    // Act
+                    val result = repository.createMemo(memoInfo.title, memoInfo.content)
+
+                    // Assert
+                    result.shouldBeInstanceOf<Result.Failure>()
+                    result.errorMessage.message shouldBe "Unknown error"
+                }
+            }
+
+            context("処理中に異常終了した場合、既定のエラーメッセージが返却されること") {
+                // Arrange
+                coEvery {  mockApi.createMemo(any(), any()) } throws RuntimeException("Network error")
+
+                // Act
+                val result = repository.createMemo(memoInfo.title, memoInfo.content)
+
+                // Assert
+                result.shouldBeInstanceOf<Result.Failure>()
+                result.errorMessage.message shouldBe "Unknown error"
+            }
+        }
+    }
 
 })
